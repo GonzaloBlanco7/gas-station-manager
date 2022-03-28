e2934c9929c720fa785c15838e4eacdcf86fd5bf
Gonzalo Blanco <gonzalo.br@outlook.com>
2022-03-28 23:03:13 +0200

    Icon + Title

--- map.js
+++ map.js
@@ -1,11 +1,10 @@
 const OSM_layer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-var map = L.map('map').setView([40.416775,-3.703790], 5);
+var map = L.map('map').setView([40.416775,-3.703790], 6);
 
 L.tileLayer(OSM_layer, {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker([40.416775,-3.703790]).addTo(map)
-    .bindPopup('Madrid')
-    .openPopup();
+    .bindPopup('Madrid');
